b5be5e7083e24db14a16e54b6b5044249c229bad
vomeajuda <leogugo9@hotmail.com>
2025-02-25 15:19:25 -0300

    bruh

---
+++ moduloD.js
@@ -0,0 +1,8 @@
+const alunos = [
+    {"rm": 1, "nome": "Leonardo", "media": 7.1, "situacao": "falecido"},
+    {"rm": 2, "nome": "Rafael", "media": 5.9, "situacao": "finalizado"},
+    {"rm": 3, "nome": "Laura", "media": 9.8, "situacao": "cursando"},
+    {"rm": 4, "nome": "Galera", "media": 2, "situacao": "quase_cursando"},
+]
+
+export default () => alunos;
